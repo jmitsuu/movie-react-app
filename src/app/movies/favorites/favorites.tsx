78f682f9231d686294d08aa2ffe1736a60425d39
unknown <jmiguelafonso@hotmail.com>
2025-03-16 15:26:50 -0300

    feat: add delete favorite

--- src/app/movies/favorites/favorites.tsx
+++ src/app/movies/favorites/favorites.tsx
@@ -1,9 +1,9 @@
 import Card from "@/app/components/card/card";
 import LayoutCard from "@/layouts/layout.card";
 import { useModelFavorites } from "./model.favorites";
-
+import { IoCloseCircle } from "react-icons/io5";
 export default function Favorites() {
- const { data } = useModelFavorites();
+ const { data, actions } = useModelFavorites();
  return (
   <section className="mt-20">
    <h1 className="text-center text-white md:text-6xl text-2xl uppercase pb-10">
@@ -12,7 +12,17 @@ export default function Favorites() {
    {data?.dataFavorites.length >= 1 ? (
     <LayoutCard>
      {data.dataFavorites?.map((movie) => {
-      return <Card key={movie.id} movie={movie} />;
+      return (
+       <div className="relative">
+        <Card key={movie.id} movie={movie} />
+        <IoCloseCircle
+         onClick={() => {
+          actions.removeMovieFavorite(movie.id);
+         }}
+         className="size-8  text-white cursor-pointer absolute right-2 top-2 z-50"
+        />
+       </div>
+      );
      })}
     </LayoutCard>
    ) : (
--- src/app/movies/favorites/model.favorites.ts
+++ src/app/movies/favorites/model.favorites.ts
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
 import { TMovie } from "../movie.type";
+import { toast } from "sonner";
 
 export function useModelFavorites() {
  const [dataFavorites, setDataFavorites] = useState<TMovie[]>([]);
-
+ const removeMovieFavorite = (id: number) => {
+  const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+  const newFav = favorites.filter((movie: { id: number }) => movie.id !== id);
+  if(newFav){
+    toast.success("Filme removido dos favoritos.")
+  }
+  setDataFavorites(newFav);
+  localStorage.setItem("favorites", JSON.stringify(newFav));
+ };
  useEffect(() => {
   const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
   setDataFavorites(favorites);
  }, []);
  return {
   data: { dataFavorites },
+  actions: { removeMovieFavorite },
  };
 }
